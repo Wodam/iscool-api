77fc54babbd3bac69480bb2bc301fe1e1a7576a7
guigawiller <guilhermewiller@hotmail.com>
2019-04-25 10:33:32 -0300

    translate migrations to english

---
+++ app/database/migrations/20190411171202-create-question.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+		const Question = queryInterface.createTable('Question', {
+			id_question: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			img_question: {
+				type: DataTypes.BLOB
+			},
+			text_question: {
+				type: DataTypes.TEXT,
+				allowNull: false
+			},
+			description_question: {
+				type: DataTypes.TEXT
+			}
+		});
+
+		Question.associate = models => {
+			Question.hasMany(models.Alternativa, {
+				as: 'alternativas',
+				foreignKey: 'id_question'
+			});
+		}
+
+		return Question;
+  },
+
+  down: (queryInterface, DataTypes) => {
+		return queryInterface.dropTable('Question')
+  }
+};
