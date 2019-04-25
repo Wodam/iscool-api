4ee2c9cee361c4b5773b0f134e3de852894e2779
guigawiller <guilhermewiller@hotmail.com>
2019-04-25 12:08:59 -0300

    github 2 x guigawiller 0

--- app/models/index.js
+++ app/models/index.js
@@ -2,36 +2,36 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../configs/database.json')[env];
-const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+class Models {
+	// --- Import all models ---
+	importAll (sequelize) {
+		return new Promise((resolve, reject) => {
+			try {
+				this.models = {};
+
+				fs
+					.readdirSync(__dirname)
+					.filter(file => (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js'))
+					.forEach(file => {
+						const model = sequelize['import'](path.join(__dirname, file));
+						this.models[model.name] = model;
+					});
+
+				Object.keys(this.models).forEach(modelName => {
+					if (this.models[modelName].associate) {
+						this.models[modelName].associate(this.models);
+					}
+				});
+
+				console.log('Models loaded :)');
+				resolve(this.models);
+			} catch (e) {
+				console.error('Error on models load :(');
+				reject(e)
+			}
+		});
+	}
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = new Models();
