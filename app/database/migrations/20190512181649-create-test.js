2654f0e29c4cfb641bd98dce22f2097323164129
guigawiller <guilhermewiller@hotmail.com>
2019-05-12 15:19:05 -0300

    Creates model and migration for Test [+]

---
+++ app/database/migrations/20190512181649-create-test.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Test', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_teste: {
+        type: Sequelize.INTEGER
+      },
+      description_test: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Test');
+  }
+};
